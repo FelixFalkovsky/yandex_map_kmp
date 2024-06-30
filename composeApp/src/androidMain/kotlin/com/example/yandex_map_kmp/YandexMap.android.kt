3a7adfc7da6a9caa8afe3f584053784642843165
FelixFalkovsky <56316435+FelixFalkovsky@users.noreply.github.com>
2024-06-30 22:11:33 +0300

    add android view

--- composeApp/src/androidMain/kotlin/com/example/yandex_map_kmp/YandexMap.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/yandex_map_kmp/YandexMap.android.kt
@@ -1,5 +1,18 @@
 package com.example.yandex_map_kmp
 
+import android.view.View
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
 @Composable
 actual fun MapScreen() {
+
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            View(context)
+        }
+    )
 }
