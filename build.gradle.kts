22bba55ee3d494f94020468f35bfbb7b322c4d2a
FelixFalkovsky <ffalkovskiy@synergy.ru>
2024-07-01 19:59:43 +0300

    update settings project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.kotlin).apply(false)
+    alias(libs.plugins.multiplatform).apply(false)
     alias(libs.plugins.android).apply(false)
     alias(libs.plugins.cocoapods).apply(false)
     alias(libs.plugins.compose.core).apply(false)
